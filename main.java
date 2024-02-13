07c2ee347d6f2f93cf9e48bc4e334b5cc95731e5
Peashooter243 <alejandrobanda5604@gmail.com>
2024-02-12 22:16:50 -0500

    I don't get this one

--- main.java
+++ main.java
@@ -1,6 +1,6 @@
 // public static void main in Java
 
-public class Main {
+public class main {
   
   public static void main(String[] args) {
     int n = 1;
@@ -9,7 +9,8 @@ public class Main {
     while(n <= 10) {
       sum = sum + n;
 
-      if(args.length != 0 &&  /* TO COMPLETE */) {
+      //I don't really get this
+      if(args.length != 0 &&  args.length < 0) {
         System.out.println("n = " + n);
         System.out.println("sum = " + sum);
       }
